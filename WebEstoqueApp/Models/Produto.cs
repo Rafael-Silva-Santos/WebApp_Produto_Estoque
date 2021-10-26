229c5bb27ce39839041be29b3e9a24a5695df61c
Rafael Santos Silva <rafasaxsofone_@hotmail.com>
2021-10-26 20:53:21 -0300

    Primeiro commit Deashboard

---
+++ WebEstoqueApp/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEstoqueApp.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string tipo { get; set; }
+        public string setor { get; set; }
+        public string fornecedor { get; set; }
+        public string local { get; set; }
+        public string quantidade { get; set; }
+    }
+}
